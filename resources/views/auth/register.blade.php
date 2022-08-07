d92af1d56571794738f9454594910c209542ff08
jquevedo82 <93448578+jquevedo82@users.noreply.github.com>
2022-08-07 19:10:38 -0300

    BS-4_register_user
    create a user in the database

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Registro</title>
+
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" />
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container center">
+
+        <div class="mt-3 text-center">
+            <div class="card border-dark mb-3" style="max-width: 18rem;">
+                <form action="{{ route('register') }}" method="post">
+                    @csrf
+
+                    <div class="card-header"><h5>Register</h5></div>
+                    <div class="card-body text-dark">
+                        <h5 class="card-title">New User</h5>
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="name" placeholder="UserName"
+                                value="{{ old('name') }}">
+                            @error('name')
+                                <div>{{ $message }}</div>
+                            @enderror
+
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-control" name="email" placeholder="Email"
+                                value="{{ old('email') }}">
+                            @error('email')
+                                <div>{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" name="password" placeholder="Password">
+                        </div>
+                        @error('password')
+                            <div>{{ $message }}</div>
+                        @enderror
+                        <div class="form-group">
+                            <input type="password" class="form-control" name="password_confirmation"
+                                placeholder="Confirm Password">
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-success btn-lg btn-block">Register</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
