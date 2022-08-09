52c99235d698b67683daf28ca314225bb4b9cb92
jquevedo82 <93448578+jquevedo82@users.noreply.github.com>
2022-08-08 21:40:48 -0300

    style: remove breaks and improve code

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
 
-    public function up():void
+    public function up(): void
     {
         Schema::create('password_resets', function (Blueprint $table) {
             $table->string('email')->index();
