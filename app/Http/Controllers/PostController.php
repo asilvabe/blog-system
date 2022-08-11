5a05d4a4e671b20ad07846fecc3b72558d5bb0d4
Alejandro Silva Berbesí <me@asilvabe.dev>
2022-08-11 08:25:38 -0500

    style: format code according to PSR

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,23 +6,29 @@ use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use App\Models\Post;
 use Illuminate\View\View;
+
 class PostController extends Controller
 {
     public function create(): View
     {
         return view('posts.create');
     }
+
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-           'title' => ['required','min:1','max:30'],
-            'body' => ['required','max:250'],
+            'title' => ['required', 'min:1', 'max:30'],
+            'body' => ['required', 'max:250'],
         ]);
+
         $post = new Post;
         $post->title = $request->input('title');
         $post->body = $request->input('body');
+
         $post->save();
+
         session()->flash('status', 'Post Created!!');
+
         return to_route('posts.create');
     }
 }
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -21,30 +21,23 @@
         </div>
         @endif
         <div class="card-body " >
-          <h5 class="card-title">Create Post</h5>
-          <p class="card-text col-md-4">
+            <h5 class="card-title">Create Post</h5>
+            <p class="card-text col-md-4">
             <div class="form-group">
                 <label for="title" class="form-label">Title</label>
-                <input type="text" class="form-control" id="title" name="title" placeholder="Write text here" value="{{old('title')}}">
-                @error('title')
-                    <small class="alert-danger">{{$message}}</small>
-                @enderror
+                <input type="text" class="form-control" id="title" name="title" placeholder="Write text here" value="{{ old('title') }}">
+                @error('title')<small class="alert-danger">{{ $message }}</small>@enderror
             </div>
             <div class="form-group">
                 <label for="body" class="form-label">Body</label>
-                <textarea name="body" class="form-control" placeholder="Write body test here" >{{old('body')}}</textarea>
-                @error('body')
-                <small class="alert-danger">{{$message}}</small>
-                @enderror
+                <textarea name="body" class="form-control" placeholder="Write body test here">{{ old('body') }}</textarea>
+                @error('body')<small class="alert-danger">{{ $message }}</small>@enderror
             </div>
-          <button type="submit" class="btn btn-primary">Save post</button>
-          <input  type="reset" class="btn btn-warning" value="Reset" />
+            <button type="submit" class="btn btn-primary">Save post</button>
+            <input  type="reset" class="btn btn-warning" value="Reset" />
       </div>
     </div>
     </div>
     </form>
 </body>
 </html>
-
-
-
