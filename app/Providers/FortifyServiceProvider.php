24125267902b4306f5910e32cb15b4e550bc27a5
Alejandro Silva Berbesí <me@asilvabe.dev>
2022-08-12 18:49:10 -0500

    feat: remove 2fa

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -43,11 +43,7 @@ class FortifyServiceProvider extends ServiceProvider
         RateLimiter::for('login', function (Request $request) {
             $email = (string) $request->email;
 
-            return Limit::perMinute(5)->by($email.$request->ip());
-        });
-
-        RateLimiter::for('two-factor', function (Request $request) {
-            return Limit::perMinute(5)->by($request->session()->get('login.id'));
+            return Limit::perMinute(3)->by($email.$request->ip());
         });
     }
 }
