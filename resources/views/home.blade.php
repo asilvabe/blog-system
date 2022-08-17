9435ac377c5b80c9e97dcd24a7038d50eee66cf3
Alejandro Silva Berbesí <me@asilvabe.dev>
2022-08-17 05:44:59 -0500

    feat: move status message to welcome view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,4 @@
 {{ auth()->user() }}
-@if(session('status'))
-    <div class="card-header">
-        {{ session('status') }}
-    </div>
-@endif
 <form action="{{ route('logout') }}" method="post">
     @csrf
     <button type="submit">Salir</button>
