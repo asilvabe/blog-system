5341fbd7ed49a5db55ea341499226f378a451577
Victor Hernandez <hernandez.victormanuel@gmail.com>
2022-08-18 08:06:17 -0400

    feat: add iinfo to show view

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Post Show</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>{{$post->title}}</h1>
+                <p>{{$post->body}}</p>
+                <p>{{$post->created_at->format('H:i  d / m / Y')}}</p>
+                <a href ="{{ route('posts.index') }}">Regresar</a>
+            </div>
+        </div>
+</div>
+</body>
+</html>
