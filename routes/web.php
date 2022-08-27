3e9e06746a94861583a9b4207842089b286ab0db
Victor Hernandez <hernandez.victormanuel@gmail.com>
2022-08-27 08:35:26 -0400

    style: del slash from route

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', [PostController::class, 'index'])->name('posts.index');
 Route::middleware(['auth'])->group(function () {
     Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
     Route::post('posts', [PostController::class, 'store'])->name('posts.store');
-    Route::get('users/', [UserController::class,'index'])->name('users.index');
+    Route::get('users', [UserController::class,'index'])->name('users.index');
 });
 
 Route::get('posts/{post}', [PostController::class,'show'] )->name('posts.show');
